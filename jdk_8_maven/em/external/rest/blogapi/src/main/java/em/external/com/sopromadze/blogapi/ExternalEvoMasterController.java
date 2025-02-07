9de27cd98f76ef618753cd419200925c991f6855
arcuri82 <arcuri82@gmail.com>
2025-02-07 21:58:51 +0100

    extenral driver

---
+++ jdk_8_maven/em/external/rest/blogapi/src/main/java/em/external/com/sopromadze/blogapi/ExternalEvoMasterController.java
@@ -0,0 +1,226 @@
+package em.external.com.sopromadze.blogapi;
+
+import org.evomaster.client.java.controller.ExternalSutController;
+import org.evomaster.client.java.controller.InstrumentedSutStarter;
+import org.evomaster.client.java.controller.api.dto.SutInfoDto;
+import org.evomaster.client.java.controller.api.dto.auth.AuthenticationDto;
+import org.evomaster.client.java.controller.api.dto.database.schema.DatabaseType;
+import org.evomaster.client.java.controller.problem.ProblemInfo;
+import org.evomaster.client.java.controller.problem.RestProblem;
+import org.evomaster.client.java.sql.DbSpecification;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExternalEvoMasterController extends ExternalSutController {
+
+
+    public static void main(String[] args) {
+
+        int controllerPort = 40100;
+        if (args.length > 0) {
+            controllerPort = Integer.parseInt(args[0]);
+        }
+        int sutPort = 12345;
+        if (args.length > 1) {
+            sutPort = Integer.parseInt(args[1]);
+        }
+        String jarLocation = "cs/rest/original/blogapi/target";
+        if (args.length > 2) {
+            jarLocation = args[2];
+        }
+        if(! jarLocation.endsWith(".jar")) {
+            jarLocation += "/blogapi-sut.jar";
+        }
+
+        int timeoutSeconds = 120;
+        if(args.length > 3){
+            timeoutSeconds = Integer.parseInt(args[3]);
+        }
+        String command = "java";
+        if(args.length > 4){
+            command = args[4];
+        }
+
+
+        ExternalEvoMasterController controller =
+                new ExternalEvoMasterController(controllerPort, jarLocation,
+                        sutPort, timeoutSeconds, command);
+        InstrumentedSutStarter starter = new InstrumentedSutStarter(controller);
+
+        starter.start();
+    }
+
+    private final int timeoutSeconds;
+    private final int sutPort;
+    private  String jarLocation;
+    private Connection sqlConnection;
+    //private String INIT_DB_SCRIPT_PATH = "/populateDB.sql";
+
+    private List<DbSpecification> dbSpecification;
+
+    private String initSQLScript;
+
+    private static final GenericContainer mysql = new GenericContainer("mysql:8.0" )
+            .withEnv(new HashMap<String, String>(){{
+                put("MYSQL_ROOT_PASSWORD", "root");
+                put("MYSQL_DATABASE", "blogapi");
+            }})
+            .withExposedPorts(3306)
+            .withTmpFs(Collections.singletonMap("/var/lib/mysql", "rw"))
+            .withClasspathResourceMapping("blogapi.sql", "/docker-entrypoint-initdb.d/blogapi.sql", BindMode.READ_ONLY)
+            .waitingFor(Wait.forLogMessage(".*MySQL init process done. Ready for start up.*", 1))
+            ;
+
+
+    public ExternalEvoMasterController(){
+        this(40100, "../core/target", 12345, 120, "java");
+    }
+
+    public ExternalEvoMasterController(String jarLocation) {
+        this();
+        this.jarLocation = jarLocation;
+    }
+
+    public ExternalEvoMasterController(
+            int controllerPort, String jarLocation, int sutPort, int timeoutSeconds, String command
+           ) {
+
+        if(jarLocation==null || jarLocation.isEmpty()){
+            throw new IllegalArgumentException("Missing jar location");
+        }
+
+
+        this.sutPort = sutPort;
+        this.jarLocation = jarLocation;
+        this.timeoutSeconds = timeoutSeconds;
+        setControllerPort(controllerPort);
+        setJavaCommand(command);
+    }
+
+
+    @Override
+    public String[] getInputParameters() {
+        return new String[]{
+                "--server.port=" + sutPort,
+                "--spring.datasource.url=" + dbUrl()
+        };
+    }
+
+    public String[] getJVMParameters() {
+        return new String[]{};
+    }
+
+    private String dbUrl() {
+
+        String host = mysql.getContainerIpAddress();
+        int port = mysql.getMappedPort(3306);
+
+        String url = "jdbc:mysql://"+host+":"+port+"/blogapi?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+        return url;
+    }
+
+    @Override
+    public String getBaseURL() {
+        return "http://localhost:" + sutPort;
+    }
+
+    @Override
+    public String getPathToExecutableJar() {
+        return jarLocation;
+    }
+
+    @Override
+    public String getLogMessageOfInitializedServer() {
+        return "Started BlogApiApplication in";
+    }
+
+    @Override
+    public long getMaxAwaitForInitializationInSeconds() {
+        return timeoutSeconds;
+    }
+
+    @Override
+    public void preStart() {
+        mysql.start();
+    }
+
+    @Override
+    public void postStart() {
+        closeDataBaseConnection();
+
+        try {
+            sqlConnection = DriverManager.getConnection(dbUrl(), "root", "root");
+
+            dbSpecification = Arrays.asList(new DbSpecification(DatabaseType.MYSQL,sqlConnection));
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void resetStateOfSUT() {
+
+    }
+
+    @Override
+    public void preStop() {
+        closeDataBaseConnection();
+    }
+
+    @Override
+    public void postStop() {
+        mysql.stop();
+    }
+
+    private void closeDataBaseConnection() {
+        if (sqlConnection != null) {
+            try {
+                sqlConnection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            sqlConnection = null;
+        }
+    }
+
+    @Override
+    public String getPackagePrefixesToCover() {
+        return "com.sopromadze.blogapi.";
+    }
+
+    @Override
+    public ProblemInfo getProblemInfo() {
+        return new RestProblem(
+                getBaseURL() + "/blogapi.json",
+                null
+        );
+    }
+
+    @Override
+    public SutInfoDto.OutputFormat getPreferredOutputFormat() {
+        return SutInfoDto.OutputFormat.JAVA_JUNIT_4;
+    }
+
+
+    @Override
+    public List<AuthenticationDto> getInfoForAuthentication() {
+        //TODO will need to setup an admin and a user
+        return null;
+    }
+
+    @Override
+    public List<DbSpecification> getDbSpecifications() {
+        return dbSpecification;
+    }
+}
