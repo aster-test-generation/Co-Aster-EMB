16fff25f1bd239d7680685413a4a7720e4ec2af2
arcuri82 <arcuri82@gmail.com>
2025-02-10 12:46:56 +0100

    fixed script

--- scripts/dist.py
+++ scripts/dist.py
@@ -257,7 +257,7 @@ def build_jdk_21_maven():
     folder = "jdk_21_maven"
     callMaven(folder, JAVA_HOME_21)
 
-    copy(folder + "/cs/rest/person-controller/target/person-controller.jar", DIST)
+    copy(folder + "/cs/rest/person-controller/target/person-controller-sut.jar", DIST)
     copy(folder + "/em/external/rest/person-controller/target/person-controller-evomaster-runner.jar", DIST)
 
 
@@ -318,57 +318,57 @@ def build_jdk_17_gradle():
 
 
 # Building JavaScript projects
-def buildJS(path, name):
-    print("Building '" + name + "' from " + path)
-    # we use "ci" instead of "install" due to major flaws in NPM
-    res = run(["npm", "ci"], shell=SHELL, cwd=path).returncode
-    if res != 0:
-        print("\nERROR installing packages with NPM in " + path)
-        exit(1)
-    res = run(["npm", "run", "em:build"], shell=SHELL, cwd=path).returncode
-    if res != 0:
-        print("\nERROR when building " + path)
-        exit(1)
-
-    target = os.path.join(DIST, name)
-    # shutil.make_archive(base_name=target, format='zip', root_dir=path+"/..", base_dir=name)
-    copytree(path, target)
-
-
-####################
-def build_js_npm():
-    buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "ncs")), "js-rest-ncs")
-    buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "scs")), "js-rest-scs")
-    buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "cyclotron")), "cyclotron")
-    buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "disease-sh-api")), "disease-sh-api")
-    buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "realworld-app")), "realworld-app")
-    buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "spacex-api")), "spacex-api")
-    buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "graphql", "react-finland")), "react-finland")
-    buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "graphql", "ecommerce-server")), "ecommerce-server")
+# def buildJS(path, name):
+#     print("Building '" + name + "' from " + path)
+#     # we use "ci" instead of "install" due to major flaws in NPM
+#     res = run(["npm", "ci"], shell=SHELL, cwd=path).returncode
+#     if res != 0:
+#         print("\nERROR installing packages with NPM in " + path)
+#         exit(1)
+#     res = run(["npm", "run", "em:build"], shell=SHELL, cwd=path).returncode
+#     if res != 0:
+#         print("\nERROR when building " + path)
+#         exit(1)
+#
+#     target = os.path.join(DIST, name)
+#     # shutil.make_archive(base_name=target, format='zip', root_dir=path+"/..", base_dir=name)
+#     copytree(path, target)
 
 
 ####################
-def build_dotnet_3():
-    env_vars = os.environ.copy()
-    folder = "dotnet_3"
+# def build_js_npm():
+#     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "ncs")), "js-rest-ncs")
+#     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "scs")), "js-rest-scs")
+#     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "cyclotron")), "cyclotron")
+#     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "disease-sh-api")), "disease-sh-api")
+#     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "realworld-app")), "realworld-app")
+#     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "rest", "spacex-api")), "spacex-api")
+#     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "graphql", "react-finland")), "react-finland")
+#     buildJS(os.path.abspath(os.path.join(PROJ_LOCATION, "js_npm", "graphql", "ecommerce-server")), "ecommerce-server")
 
-    dotnet = run(["dotnet", "build"], shell=SHELL, cwd=os.path.join(PROJ_LOCATION, folder), env=env_vars)
 
-    if dotnet.returncode != 0:
-        print("\nERROR: .Net command failed")
-        exit(1)
-
-    rest = os.path.join(PROJ_LOCATION, "dotnet_3", "em", "embedded", "rest")
-
-    ncs = os.path.abspath(os.path.join(rest, "NcsDriver", "bin", "Debug", "netcoreapp3.1"))
-    scs = os.path.abspath(os.path.join(rest, "ScsDriver", "bin", "Debug", "netcoreapp3.1"))
-    sampleproject = os.path.abspath(os.path.join(rest, "SampleProjectDriver", "bin", "Debug", "netcoreapp3.1"))
-    menuapi = os.path.abspath(os.path.join(rest, "MenuAPIDriver", "bin", "Debug", "netcoreapp3.1"))
-
-    copytree(ncs, os.path.join(DIST, "cs-rest-ncs"))
-    copytree(scs, os.path.join(DIST, "cs-rest-scs"))
-    copytree(sampleproject, os.path.join(DIST, "sampleproject"))
-    copytree(menuapi, os.path.join(DIST, "menu-api"))
+####################
+# def build_dotnet_3():
+#     env_vars = os.environ.copy()
+#     folder = "dotnet_3"
+#
+#     dotnet = run(["dotnet", "build"], shell=SHELL, cwd=os.path.join(PROJ_LOCATION, folder), env=env_vars)
+#
+#     if dotnet.returncode != 0:
+#         print("\nERROR: .Net command failed")
+#         exit(1)
+#
+#     rest = os.path.join(PROJ_LOCATION, "dotnet_3", "em", "embedded", "rest")
+#
+#     ncs = os.path.abspath(os.path.join(rest, "NcsDriver", "bin", "Debug", "netcoreapp3.1"))
+#     scs = os.path.abspath(os.path.join(rest, "ScsDriver", "bin", "Debug", "netcoreapp3.1"))
+#     sampleproject = os.path.abspath(os.path.join(rest, "SampleProjectDriver", "bin", "Debug", "netcoreapp3.1"))
+#     menuapi = os.path.abspath(os.path.join(rest, "MenuAPIDriver", "bin", "Debug", "netcoreapp3.1"))
+#
+#     copytree(ncs, os.path.join(DIST, "cs-rest-ncs"))
+#     copytree(scs, os.path.join(DIST, "cs-rest-scs"))
+#     copytree(sampleproject, os.path.join(DIST, "sampleproject"))
+#     copytree(menuapi, os.path.join(DIST, "menu-api"))
 
 
 ######################################################################################
