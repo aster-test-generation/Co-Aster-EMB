9a8b372ee2b21fc4be02316c1979b0d722bbfeee
arcuri82 <arcuri82@gmail.com>
2025-02-17 12:09:45 +0100

    starting createDocker script

---
+++ scripts/createDockerFiles.py
@@ -0,0 +1,10 @@
+
+
+# should be callable without ANY input
+# execute whatever is in "dockerize"
+### for example, read EMB/scripts/dockerize/data/sut.csv for list of names, and run docker-generator/py on each of them
+# outPut of files should be under EMB/dockerize
+# no Docker should be run, just creating files
+# can assume EMB/dist has already been created (do not run it here)
+# then, output under EMB/dockerize must be put under Git
+# additional files links must point correctly, do not duplicate files
