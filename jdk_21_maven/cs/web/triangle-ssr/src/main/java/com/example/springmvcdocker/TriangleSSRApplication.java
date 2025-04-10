6bc1a319374eee195a28703f40debb119e9ea397
Gebremariam Mesfin Assres <mesfin.assres@gmail.com>
2025-04-10 12:36:57 +0200

    Updates on TriangleClassification

--- jdk_21_maven/cs/web/triangle-ssr/src/main/java/com/example/springmvcdocker/TriangleSSRApplication.java
+++ jdk_21_maven/cs/web/triangle-ssr/src/main/java/com/example/springmvcdocker/TriangleSSRApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMvcDockerApplication {
+public class TriangleSSRApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringMvcDockerApplication.class, args);
+        SpringApplication.run(TriangleSSRApplication.class, args);
     }
 }
